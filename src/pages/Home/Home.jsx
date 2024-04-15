c1c2f319aeb8b83bc87c471146612f72c742cce4
Anowar1987 <anowarhossen.iim@gmail.com>
2024-04-15 14:14:36 +0600

    navbar completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ import Slider from "../slider/Slider";
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl font-poppins">This is Home</h2>
             <Slider></Slider>
             <Estate></Estate>
             <LatestNews></LatestNews>
--- src/pages/slider/Slider.jsx
+++ src/pages/slider/Slider.jsx
@@ -2,7 +2,7 @@
 
 const Slider = () => {
     return (
-        <div>
+        <div className="mt-8">
             <h2 className="text-4xl font-bold">This is slider</h2>
         </div>
     );
