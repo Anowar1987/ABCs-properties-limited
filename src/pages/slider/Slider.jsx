9f20f616de90390ff2d4d5778f5c62bd0bd3ebf0
Anowar1987 <anowarhossen.iim@gmail.com>
2024-04-15 19:15:39 +0600

    slider added

--- src/pages/slider/Slider.jsx
+++ src/pages/slider/Slider.jsx
@@ -1,9 +1,41 @@
-
+import sliderImage1 from '../../assets/Images/banner/BImage01.jpg';
+import sliderImage2 from '../../assets/Images/banner/BImage02.jpg';
+import sliderImage3 from '../../assets/Images/banner/BImage03.jpg';
+import sliderImage4 from '../../assets/Images/banner/BImage04.jpg';
 
 const Slider = () => {
     return (
         <div className="mt-8">
-            <h2 className="text-4xl font-bold">This is slider</h2>
+            <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src={sliderImage1} className="w-full h-80" />
+    <div className="absolute flex justify-between transform left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src={sliderImage2} className="w-full h-80" />
+    <div className="absolute flex justify-between transform left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src={sliderImage3} className="w-full h-80" />
+    <div className="absolute flex justify-between transform left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src={sliderImage4} className="w-full h-80" />
+    <div className="absolute flex justify-between transform left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
