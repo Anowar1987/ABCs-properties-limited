2f9a7d1992e6ba5e76a65df947dd1097a5374e01
Anowar1987 <anowarhossen.iim@gmail.com>
2024-04-16 23:31:39 +0600

    Whats sets section completed

--- src/pages/WhatSets/WhatSets.jsx
+++ src/pages/WhatSets/WhatSets.jsx
@@ -1,9 +1,38 @@
 
 const WhatSets = () => {
     return (
-        <div>
-            <h2 className="text-4xl">This is whats sets us apart</h2>
+        <div className="mt-10 mb-10">
+            <h2 className="text-4xl font-bold text-center">Whats sets us apart</h2>
+            <div className="grid grid-cols-3 gap-5">
+                <div className="w-full bg-slate-300 p-4 rounded-2xl">
+                    <h3 className="text-3xl font-bold text-center">Experience</h3>
+                    <p className="font-semibold mt-3">We have over 30 years of experience in the construction and real estate sector that cannot be matched by any other developer. ABCs is the only developer that has received the Daily Star DHL "Enterprise of the Year 2000 Award". In recognition of outstanding leadership quality and as a role model in corporate business in Bangladesh.</p>
+                </div>
+                <div className="w-full bg-slate-300 p-4 rounded-2xl mt-6">
+                    <h3 className="text-3xl font-bold text-center">Credit rating</h3>
+                    <p className="font-semibold mt-3">ABCS is rated AA+ by Credit Rating agencies. Our success is due to our prudent approach to business, our dedicated team of management professionals and our loyal customers.</p>
+                </div>
+                <div className="w-full bg-slate-300 p-4 rounded-2xl mt-6">
+                    <h3 className="text-3xl font-bold text-center">Trust</h3>
+                    <p className="font-semibold mt-3">ABCs has handed over more than 10,000 units and over 1200 projects. We completed the Indoor Stadium in Mirpur in months. We believe that timely delivery and quality of work should be the most important goal of any developer.</p>
+                </div>
+                <div className="w-full bg-slate-300 p-4 rounded-2xl mt-6">
+                    <h3 className="text-3xl font-bold text-center">Quality</h3>
+                    <p className="font-semibold mt-3">We pride ourselves on the fact that we never compromise on quality or safety. Our finishing is one of the best. All our concreting comes from computerized batching plants. Our engineering, design and construction teams are some of the most experienced in the country. We incorporate all design safety factors. We never compromise on quality and safety and that is why our apartments are so sought after.</p>
+                </div>
+                <div className="w-full bg-slate-300 p-4 rounded-2xl mt-6">
+                    <h3 className="text-3xl font-bold text-center">Design</h3>
+                    <p className="font-semibold mt-3">We are what we repeatedly do. Excellence, then, is not an act, but a habit. We work with some of the best consultants (local and foreign) to create some of the most stunning projects in the country.
+                    </p>
+                </div>
+                <div className="w-full bg-slate-300 p-4 rounded-2xl mt-6">
+                    <h3 className="text-3xl font-bold text-center">Our Achievement</h3>
+                    <p className="font-semibold mt-3">We achievement many national and international award. These award increse our duty to the society</p>
+                </div>
+
+            </div>
         </div>
+
     );
 };
 
